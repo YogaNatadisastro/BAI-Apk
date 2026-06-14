dabbbfb5f6addb1b5e025324253bc08292aa6036
yoganatadisastro <yoganatadisastro@gmail.com>
2026-06-14 15:35:45 +0700

    Added a few configuration at AdminBaseActivity, fragment_assessment.xml, at all

--- app/src/main/java/com/project/bai_app/ui/adminPage/BaseAdminActivity.kt
+++ app/src/main/java/com/project/bai_app/ui/adminPage/BaseAdminActivity.kt
@@ -1,7 +1,6 @@
 package com.project.bai_app.ui.adminPage
 
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -21,19 +20,30 @@ class BaseAdminActivity : AppCompatActivity() {
         bind = ActivityBaseAdminBinding.inflate(layoutInflater)
         setContentView(bind.root)
 
+        setupWindowInsets()
+        setupNavigation()
+    }
+
+    private fun setupWindowInsets() {
         ViewCompat.setOnApplyWindowInsetsListener(bind.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
 
+    private fun setupNavigation() {
         val navHostFragment = supportFragmentManager
-            .findFragmentById(R.id.fragment_container_view_tag) as NavHostFragment
+            .findFragmentById(R.id.fragment_container) as NavHostFragment
         navController = navHostFragment.navController
 
-        bind.botNavbar.setOnItemSelectedListener { item ->
+        bind.bottomNavigation.setOnItemSelectedListener { item ->
             val currentDestination = navController.currentDestination?.id
-            val navOptionsBuilder = androidx.navigation.navOptions {
+            
+            // Avoid re-navigating to the same destination
+            if (currentDestination == item.itemId) return@setOnItemSelectedListener false
+
+            val navOptions = androidx.navigation.navOptions {
                 anim {
                     enter = R.anim.slide_in_right
                     exit = R.anim.slide_out_left
@@ -41,17 +51,39 @@ class BaseAdminActivity : AppCompatActivity() {
                     popExit = R.anim.slide_out_right
                 }
                 launchSingleTop = true
+                popUpTo(navController.graph.startDestinationId) {
+                    saveState = true
+                }
+                restoreState = true
             }
 
             when (item.itemId) {
-                R.id.home -> {
-                    if (currentDestination != R.id.homeAdminFragment) {
-                        navController.navigate(R.id.homeAdminFragment, null, navOptionsBuilder)
-                    }
+                R.id.homeAdminFragment -> {
+                    navController.navigate(R.id.homeAdminFragment, null, navOptions)
+                    true
+                }
+                R.id.gad7Fragment -> {
+                    navController.navigate(R.id.gad7Fragment, null, navOptions)
                     true
                 }
                 else -> false
             }
         }
+        
+        // Synchronize bottom navigation selection with current destination
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.homeAdminFragment -> {
+                    bind.bottomNavigation.menu.findItem(R.id.homeAdminFragment)?.isChecked = true
+                }
+                R.id.gad7Fragment, R.id.hadsFragment, R.id.resultFragment -> {
+                    bind.bottomNavigation.menu.findItem(R.id.gad7Fragment)?.isChecked = true
+                }
+            }
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
