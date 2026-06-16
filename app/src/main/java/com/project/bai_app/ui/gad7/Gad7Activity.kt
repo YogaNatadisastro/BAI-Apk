8b8118b66655db82074b10f07dbac7df1a1283d1
yoganatadisastro <yoganatadisastro@gmail.com>
2026-06-16 16:43:34 +0700

    added Gad7Activity.kt and HadsActivity.kt

---
+++ app/src/main/java/com/project/bai_app/ui/gad7/Gad7Activity.kt
@@ -0,0 +1,21 @@
+package com.project.bai_app.ui.gad7
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.project.bai_app.R
+
+class Gad7Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_gad7)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
